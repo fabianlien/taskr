b67e9c49e5b9bc99086778f86cb503d45f565b08
Fabian Lien <fabian.lien@gmail.com>
2022-06-27 10:06:13 +0000

    add image input field and update functionality to profile edit page

--- src/pages/profile/UpdateProfileForm.js
+++ src/pages/profile/UpdateProfileForm.js
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-import { useHistory, useParams } from 'react-router-dom';
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../context/CurrentUserContext';
-
+import Image from "react-bootstrap/Image";
+import { useHistory, useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../context/CurrentUserContext";
 
 const UpdateProfileForm = () => {
   const [profileData, setProfileData] = useState({
     name: "",
     bio: "",
+    profile_image: "",
   });
-  const {name, bio} = profileData
+  const { name, bio, profile_image } = profileData;
+  const imageInput = useRef(null);
   const history = useHistory();
   const currentUser = useCurrentUser();
   const { id } = useParams();
   const [errors, setErrors] = useState({});
-  
+
   useEffect(() => {
     const onMount = async () => {
       try {
         const { data } = await axiosReq.get(`/profiles/${id}`);
-        const { bio, name } = data
+        const { bio, name } = data;
         if (currentUser.pk === parseInt(id)) {
-            setProfileData({bio, name})
+          setProfileData({ bio, name });
         } else {
-            history.goBack();
+          history.goBack();
         }
       } catch (error) {
         console.log(error);
@@ -45,13 +47,23 @@ const UpdateProfileForm = () => {
     });
   };
 
+  const handleImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(profile_image);
+      setProfileData({
+        ...profileData,
+        profile_image: URL.createObjectURL(event.target.files[0])
+      })
+    }
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData();
     formData.append("name", name);
     formData.append("bio", bio);
-
+    formData.append("profile_image", imageInput.current.files[0])
 
     try {
       await axiosReq.put(`/profiles/${id}`, formData);
@@ -64,58 +76,82 @@ const UpdateProfileForm = () => {
 
   return (
     <Container style={{ width: "80%" }}>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group as={Row} controlId="name">
-        <Form.Label className="d-none mg-b-20" column sm={2}>
-          Title
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={handleChange}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="text-center">
+          {profile_image ? (
+            <>
+              <figure>
+                <Image src={profile_image} rounded />
+              </figure>
+              <Form.Label htmlFor="image-upload">Change the image</Form.Label>
+            </>
+          ) : (
+            <Form.Label
+              className="d-flex justify-content-center"
+              htmlFor="image-upload"
+            >
+              <span>Click or tap to upload an image</span>
+            </Form.Label>
+          )}
+          <Form.File
+            id="image-upload"
+            accept="image/*"
+            onChange={handleImage}
+            ref={imageInput}
           />
-        </Col>
-      </Form.Group>
-      {errors.name?.map((message, index) => (
-        <Alert variant="warning" key={index}>
-          {message}
-        </Alert>
-      ))}
+        </Form.Group>
 
-      <Form.Group as={Row} controlId="bio">
-        <Form.Label className="d-none mg-b-20" column sm={2}>
-          Description
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control
-            as="textarea"
-            placeholder="Write something about yourself..."
-            name="bio"
-            value={bio}
-            onChange={handleChange}
-          />
-        </Col>
-      </Form.Group>
-      {errors.bio?.map((message, index) => (
-        <Alert variant="warning" key={index}>
-          {message}
-        </Alert>
-      ))}
+        <Form.Group as={Row} controlId="name">
+          <Form.Label className="d-none mg-b-20" column sm={2}>
+            Title
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </Col>
+        </Form.Group>
+        {errors.name?.map((message, index) => (
+          <Alert variant="warning" key={index}>
+            {message}
+          </Alert>
+        ))}
 
-      <Form.Group as={Row}>
-        <Col sm={{ span: 4 }}>
-          <Button type="submit">Save</Button>
-        </Col>
-        <Col sm={{ span: 6 }}>
-          <Button onClick={() => history.push('/')}>Cancel</Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  </Container>
-  )
-}
+        <Form.Group as={Row} controlId="bio">
+          <Form.Label className="d-none mg-b-20" column sm={2}>
+            Description
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              as="textarea"
+              placeholder="Write something about yourself..."
+              name="bio"
+              value={bio}
+              onChange={handleChange}
+            />
+          </Col>
+        </Form.Group>
+        {errors.bio?.map((message, index) => (
+          <Alert variant="warning" key={index}>
+            {message}
+          </Alert>
+        ))}
 
-export default UpdateProfileForm
+        <Form.Group as={Row}>
+          <Col sm={{ span: 4 }}>
+            <Button type="submit">Save</Button>
+          </Col>
+          <Col sm={{ span: 6 }}>
+            <Button onClick={() => history.push("/")}>Cancel</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+};
+
+export default UpdateProfileForm;
